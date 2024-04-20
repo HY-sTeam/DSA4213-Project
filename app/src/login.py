e2746e5d8fdfe02d3a3ce2114a60bfeef971c676
yeonghui <e0774113@u.nus.edu>
2024-04-20 17:37:31 +0800

    included xy's fixes to include signup option within login page and fixed signup logic flaw

--- app/src/login.py
+++ app/src/login.py
@@ -7,20 +7,6 @@ import psycopg2
 import src.login_helper as lg
 import streamlit as st
 
-# # Initialize session state variables
-# if 'page' not in st.session_state:
-#     st.session_state.page = "login"
-# if 'credential_status' not in st.session_state: # comment or uncomment this both line will throw AttributeError
-#     st.session_state.credential_status = None
-# if 'otp_tbc' not in st.session_state:
-#     st.session_state.otp_tbc = None
-# if 'new_password' not in st.session_state:
-#     st.session_state.new_password = None
-# if 'confirm_password' not in st.session_state:
-#     st.session_state.confirm_password = None
-# if 'login_button' not in st.session_state:
-#     st.session_state.login_button = None
-
 # Initialize session state variables
 if 'page' not in st.session_state:
     st.session_state.page = "login"
@@ -36,25 +22,6 @@ if 'new_password' not in st.session_state:
     st.session_state.new_password = None
 if 'confirm_password' not in st.session_state:
     st.session_state.confirm_password = None
-
-# # Function to check user credentials
-# def check_credentials():
-#     if st.session_state.login_email == '' or st.session_state.login_password == '':
-#         st.session_state.no_mail_no_pin = False
-#         return st.session_state.no_mail_no_pin
-#     else: 
-#         conn = lg.get_db_connection()
-#         cur = conn.cursor()
-#         cur.execute("SELECT * FROM Users WHERE email = %s", (st.session_state.login_email,))
-#         result = cur.fetchone()
-#         cur.close()
-#         conn.close()
-#         if result:
-#             # check user input same as db input, True if pin matches, False otherwise, store in credential_status
-#             st.session_state.credential_status = (result[2] == st.session_state.login_password)  
-#             return st.session_state.credential_status
-#         else: 
-#             return st.session_state.credential_status # Email does not exist in the database
 
 def login():
     st.title("Welcome to Presentation Generator")
@@ -73,18 +40,26 @@ def login():
             login = st.form_submit_button('Log In')
 
             if login:
-                credential_status = lg.check_credentials(st.session_state.email, st.session_state.password)
-                if credential_status is True:
-                    st.success('Logged in successfully.')
-                    st.session_state.page = "main"
-                elif credential_status is False:
-                    st.error('Wrong password. Try again.')
-                    st.session_state.page = "login"
 
-                # Edited this line to fix routing problem
+                # Check if both email and password are filled
+                if not email or not password:
+                    st.error('Please fill in all fields')
+                
                 else:
-                    st.error('Email does not exist. Proceed to signup.')
-                    st.session_state.page = "signup"
+                    credential_status = lg.check_credentials(st.session_state.email, st.session_state.password)
+                    if credential_status is True:
+                        st.success('Logged in successfully.')
+                        st.session_state.page = "main"
+                        st.rerun()
+
+                    elif credential_status is False:
+                        st.error('Wrong password. Try again.')
+                        st.session_state.page = "login"
+
+                    # Edited this line to fix routing problem
+                    else:
+                        st.error('Email does not exist. Please proceed to signup.')
+                        # st.session_state.page = "signup"
     else:
         st.success('You are already logged in! Proceeding to the main page...')
         st.session_state.page = "main"
